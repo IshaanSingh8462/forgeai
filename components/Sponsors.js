5b6d1bc5e67b022a1525ad8e7adcf026bce6e08d
IshaanSingh8462 <ishaan8462@gmail.com>
2026-09-07 00:45:51 -0400

    aops sponsor

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -36,6 +36,22 @@ export default function Sponsors() {
         </motion.div>
 
         <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true, margin: "-100px" }}
+          transition={{ duration: 0.6 }}
+          className="mx-auto mb-8 w-104 md:w-128"
+        >
+          <a href="https://aopsacademy.org/" target="_blank" rel="noopener">
+            <img
+              src="/aops-logo.png"
+              alt="Sponsor logo"
+              className="w-full h-auto object-contain"
+            />
+          </a>
+        </motion.div>
+
+        <motion.div
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, margin: "-100px" }}
